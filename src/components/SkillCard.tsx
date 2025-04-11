046fe339c96b736f987c2d0a2496c7e06a9c64c4
venub34 <venubeenaveni1734@gmail.com>
2025-04-10 20:01:01 -0400

    Update SkillCard.tsx

--- src/components/SkillCard.tsx
+++ src/components/SkillCard.tsx
@@ -9,7 +9,7 @@ const SkillCard: React.FC<Skill> = ({ link, name }) => {
   return (
     <>
       <div className="flex items-center gap-4 border px-3 py-3 border-zinc-600 rounded-md">
-        <div className="bg-[#495057] p-2 rounded-md">
+        <div className="bg-[#293343] p-2 rounded-md">
           <img src={link} width={30} height={30} alt={name} />
         </div>
         <p className="text-2xl">{name}</p>
